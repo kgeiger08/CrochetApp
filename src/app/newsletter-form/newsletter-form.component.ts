c9785f89e3bcc117c8b09537e3408bd4cabde733
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2021-01-27 17:07:58 -0600

    form created and working

--- src/app/newsletter-form/newsletter-form.component.ts
+++ src/app/newsletter-form/newsletter-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
@@ -10,6 +11,10 @@ export class NewsletterFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSignupSubmitted(form: NgForm) {
+    console.log(form);
   }
 
 }
