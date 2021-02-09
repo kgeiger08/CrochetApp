a9d159fe3f2ca493f71c6499389fa4ffe3246315
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2021-02-09 05:38:35 -0600

    updated visual inspiration component to use api.

--- src/app/visual-inspiration/visual-inspiration.component.ts
+++ src/app/visual-inspiration/visual-inspiration.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchImageService } from './fetchImage.service';
+
 
 @Component({
   selector: 'app-visual-inspiration',
   templateUrl: './visual-inspiration.component.html',
-  styleUrls: ['./visual-inspiration.component.css']
+  styleUrls: ['./visual-inspiration.component.scss']
 })
 export class VisualInspirationComponent implements OnInit {
-  pixabayURL = 'https://pixabay.com/api/?key=20209851-bb89ce49d817cc4dd29ff2685&q=crochet+yarn&image_type=photo';
-  result = document.getElementById("imgResult");
+  image: any;
+  showImage: boolean = false;
+  
+  handleImage(data) {
+    this.showImage = true;
+    let randomImage: any = data.hits[Math.floor(Math.random()*data.hits.length)].webformatURL;
+    
+    this.image = randomImage;
+    console.log(data);
+  }
 
-  constructor() { }
+  constructor(private fetchImageService: FetchImageService) { }
+
+  displayImage() {
+    return this.fetchImageService.getImage().then(Response => Response.json().then(data => {
+      this.handleImage(data);
+  })); 
+  }
 
   ngOnInit(): void {
-
-    fetch(this.pixabayURL).then(Response => Response.json().then(data => {
-      console.log(data);
-      // this.result.innerHTML = "<img class='img-responsive' src='data.id' alt='crochet image'/>";
-    })) 
 
   }
 
